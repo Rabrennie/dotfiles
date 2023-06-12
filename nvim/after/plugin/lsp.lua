77c904e8bffbbca697fef2bff4dc92074fab9b6e
Rab Rennie <rennierab@gmail.com>
2023-06-12 18:36:03 +0100

    More lsp config - formatting with null-ls

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,18 +1,20 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
-	lsp.default_keymaps({buffer = bufnr})
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    local opts = { buffer = bufnr, remap = false }
+    lsp.default_keymaps({ buffer = bufnr })
+    vim.keymap.set("n", "gd", '<cmd>Telescope lsp_definitions<cr>', opts)
+    vim.keymap.set("n", "gr", '<cmd>Telescope lsp_references<cr>', { buffer = true })
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set({ 'n', 'x' }, '<leader>f', function()
+        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+    end, opts)
 end)
 
--- (Optional) Configure lua language server for neovim
-
 lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'lua_ls'
+    'tsserver',
+    'eslint',
+    'lua_ls',
 })
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
@@ -21,11 +23,33 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-	mapping = {
-		['<Tab>'] = cmp_action.tab_complete(),
-		['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-		['<CR>'] = cmp.mapping.confirm({ select = false })
-	}
+    mapping = {
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false })
+    }
 })
 
 lsp.setup()
+
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.formatting.prettierd.with({
+            filetypes = {
+                "css",
+                "javascript",
+                "javascriptreact",
+                "typescript",
+                "typescriptreact",
+                "json",
+                "scss",
+                "less",
+                "tsx"
+            }
+        })
+    },
+    autostart = true,
+})
--- nvim/lua/rab/packer.lua
+++ nvim/lua/rab/packer.lua
@@ -10,7 +10,7 @@ return require('packer').startup(function(use)
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use({
@@ -32,19 +32,23 @@ return require('packer').startup(function(use)
         branch = 'v2.x',
         requires = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            run = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                                       -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
-    };
-}
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
+
+            -- Formatting
+            { 'jose-elias-alvarez/null-ls.nvim' },
+        },
+    }
 end)
