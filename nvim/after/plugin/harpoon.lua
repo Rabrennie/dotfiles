76548a0da3947117369e0c41c160419206c46704
Rab Rennie <rennierab@gmail.com>
2023-06-14 18:30:09 +0100

    More keybinding changes

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -5,8 +5,3 @@ require("telescope").load_extension('harpoon')
 
 vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Mark file" })
 vim.keymap.set("n", "<C-e>", "<cmd>Telescope harpoon marks<cr>", { desc = "Show harpoon marks" })
-
-vim.keymap.set("n", "<C-h>", function () ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function () ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function () ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function () ui.nav_file(4) end)
--- nvim/lua/rab/remap.lua
+++ nvim/lua/rab/remap.lua
@@ -8,14 +8,14 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text down" 
 vim.keymap.set("n", "n", "nzzzv", { desc = "Next centered" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous centered" })
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Copy line to system clipboard" })
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = "Delete without buffer" })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without buffer" })
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Substitute all" })
 
-vim.keymap.set("n", "<C-h>", "<C-W>h", { desc = "Move cursor left window" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { remap = true, desc = "Move cursor left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move cursor down window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move cursor up window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move cursor right window" })
