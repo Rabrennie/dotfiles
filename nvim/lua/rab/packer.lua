8106c68ffab167eb0685ba97cda00dfd725bbc59
rabrennie <9087372+Rabrennie@users.noreply.github.com>
2023-06-26 23:15:31 +0100

    lint packer

--- nvim/lua/rab/packer.lua
+++ nvim/lua/rab/packer.lua
@@ -1,82 +1,85 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
     -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+    use("wbthomason/packer.nvim")
 
     use({
-        'olimorris/onedarkpro.nvim',
-        config = function()
-            vim.cmd('colorscheme onedark')
-        end
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        -- or                            , branch = '0.1.x',
+        requires = { { "nvim-lua/plenary.nvim" } },
     })
 
-    use({ 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } })
-    use 'nvim-treesitter/nvim-treesitter-context'
-    use('ThePrimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use('gpanders/editorconfig.nvim')
+    use({
+        "olimorris/onedarkpro.nvim",
+        config = function()
+            vim.cmd("colorscheme onedark")
+        end,
+    })
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+    use({ "nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" } })
+    use("nvim-treesitter/nvim-treesitter-context")
+    use("ThePrimeagen/harpoon")
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use("gpanders/editorconfig.nvim")
+
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
+            { "neovim/nvim-lspconfig" }, -- Required
             {
                 -- Optional
-                'williamboman/mason.nvim',
+                "williamboman/mason.nvim",
                 run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
+                    pcall(vim.cmd, "MasonUpdate")
                 end,
             },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
+            { "hrsh7th/nvim-cmp" }, -- Required
+            { "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "L3MON4D3/LuaSnip" }, -- Required
 
             -- Formatting
-            { 'jose-elias-alvarez/null-ls.nvim' },
+            { "jose-elias-alvarez/null-ls.nvim" },
         },
-    }
+    })
 
-    use {
+    use({
         "folke/which-key.nvim",
         config = function()
             vim.o.timeout = true
             vim.o.timeoutlen = 300
-            require("which-key").setup {
+            require("which-key").setup({
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
-            }
-        end
-    }
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
+            })
+        end,
+    })
 
-    use {
+    use({
+        "numToStr/Comment.nvim",
+        config = function()
+            require("Comment").setup()
+        end,
+    })
+
+    use({
         "folke/neodev.nvim",
         config = function()
-            require('neodev').setup()
-        end
-    }
+            require("neodev").setup()
+        end,
+    })
+
     use({
         "nvim-lualine/lualine.nvim",
         requires = { "nvim-tree/nvim-web-devicons", opt = true },
