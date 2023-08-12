ed51d6b17e28099e3ab5131947c235fe17972b8a
Rab Rennie <rennierab@gmail.com>
2023-08-12 19:32:19 +0100

    Add lsp stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,177 +1,186 @@
 return {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-        -- LSP Support
-        { "neovim/nvim-lspconfig" }, -- Required
-        {
-            -- Optional
-            "williamboman/mason.nvim",
-            build = function()
-                pcall(vim.cmd, "MasonUpdate")
-            end,
-        },
-        { "williamboman/mason-lspconfig.nvim" }, -- Optional
-
-        -- Autocompletion
-        { "hrsh7th/nvim-cmp" }, -- Required
-        { "saadparwaiz1/cmp_luasnip" },
-        { "hrsh7th/cmp-buffer" }, -- Required
-        { "hrsh7th/cmp-path" }, -- Required
-        { "hrsh7th/cmp-nvim-lsp" }, -- Required
-        { "L3MON4D3/LuaSnip" }, -- Required
-
-        { "folke/neodev.nvim" }, -- Required
-
-        -- Formatting
-        { "jose-elias-alvarez/null-ls.nvim" },
+  "VonHeikemen/lsp-zero.nvim",
+  branch = "v2.x",
+  dependencies = {
+    -- LSP Support
+    { "neovim/nvim-lspconfig" }, -- Required
+    {
+      -- Optional
+      "williamboman/mason.nvim",
+      build = function()
+        pcall(vim.cmd, "MasonUpdate")
+      end,
     },
-    config = function()
-        local lsp = require("lsp-zero").preset({})
+    { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-        lsp.on_attach(function(client, bufnr)
-            local opts = { buffer = bufnr, remap = false }
-            lsp.default_keymaps({ buffer = bufnr })
-            vim.keymap.set(
-                "n",
-                "gd",
-                "<cmd>Telescope lsp_definitions<cr>",
-                { buffer = true, desc = "Go to definition" }
-            )
-            vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = true, desc = "Show references" })
-            vim.keymap.set("n", "K", function()
-                vim.lsp.buf.hover()
-            end, { buffer = bufnr, desc = "Show hover" })
-        end)
+    -- Autocompletion
+    { "hrsh7th/nvim-cmp" }, -- Required
+    { "saadparwaiz1/cmp_luasnip" },
+    { "hrsh7th/cmp-buffer" }, -- Required
+    { "hrsh7th/cmp-path" }, -- Required
+    { "hrsh7th/cmp-nvim-lsp" }, -- Required
+    { "L3MON4D3/LuaSnip" }, -- Required
 
-        lsp.ensure_installed({
-            "tsserver",
-            "eslint",
-            "lua_ls",
-        })
+    { "folke/neodev.nvim" }, -- Required
 
-        require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+    -- Formatting
+    { "jose-elias-alvarez/null-ls.nvim" },
+  },
+  config = function()
+    local lsp = require("lsp-zero").preset({})
 
-        require("lspconfig").arduino_language_server.setup({
-            cmd = {
-                "arduino-language-server",
-                "-cli-config",
-                "/Users/rab/Library/Arduino15/arduino-cli.yaml",
-                "-fqbn",
-                "esp32:esp32:esp32",
-                "-cli",
-                "/opt/homebrew/bin/arduino-cli",
-                "-clangd",
-                "/usr/bin/clangd",
-            },
-        })
+    lsp.on_attach(function(client, bufnr)
+      local opts = { buffer = bufnr, remap = false }
+      lsp.default_keymaps({ buffer = bufnr })
+      vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<cr>", { buffer = true, desc = "Go to definition" })
+      vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = true, desc = "Show references" })
+      vim.keymap.set("n", "K", function()
+        vim.lsp.buf.hover()
+      end, { buffer = bufnr, desc = "Show hover" })
+    end)
 
-        local cmp = require("cmp")
-        local cmp_action = require("lsp-zero").cmp_action()
+    lsp.ensure_installed({
+      "tsserver",
+      "eslint",
+      "lua_ls",
+    })
 
-        local ls = require("luasnip")
-        require("luasnip.loaders.from_vscode").lazy_load()
+    require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
-        ls.filetype_extend("ruby", { "rails" })
-        ls.filetype_extend("javascript", { "typescript" })
-        ls.filetype_extend("arduino", { "cpp" })
+    require("lspconfig").arduino_language_server.setup({
+      cmd = {
+        "arduino-language-server",
+        "-cli-config",
+        "/Users/rab/Library/Arduino15/arduino-cli.yaml",
+        "-fqbn",
+        "esp32:esp32:esp32",
+        "-cli",
+        "/opt/homebrew/bin/arduino-cli",
+        "-clangd",
+        "/usr/bin/clangd",
+      },
+    })
 
-        cmp.setup({
-            sources = {
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "path" },
-                { name = "buffer", keyword_length = 5 },
-            },
-            mapping = {
-                ["<Tab>"] = cmp_action.tab_complete(),
-                ["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
-                ["<CR>"] = cmp.mapping.confirm({ select = false }),
-            },
-            sorting = {
-                comparators = {
-                    cmp.config.compare.offset,
-                    cmp.config.compare.exact,
-                    cmp.config.compare.score,
+    require("lspconfig").yamlls.setup({
+      settings = {
+        yaml = {
+          schemas = {
+            ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+          },
+        },
+      },
+    })
 
-                    -- copied from cmp-under, but I don't think I need the plugin for this.
-                    -- I might add some more of my own.
-                    function(entry1, entry2)
-                        local _, entry1_under = entry1.completion_item.label:find("^_+")
-                        local _, entry2_under = entry2.completion_item.label:find("^_+")
-                        entry1_under = entry1_under or 0
-                        entry2_under = entry2_under or 0
-                        if entry1_under > entry2_under then
-                            return false
-                        elseif entry1_under < entry2_under then
-                            return true
-                        end
-                    end,
+    local cmp = require("cmp")
+    local cmp_action = require("lsp-zero").cmp_action()
 
-                    cmp.config.compare.kind,
-                    cmp.config.compare.sort_text,
-                    cmp.config.compare.length,
-                    cmp.config.compare.order,
-                },
-            },
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-        })
+    local ls = require("luasnip")
+    require("luasnip.loaders.from_vscode").lazy_load()
 
-        lsp.format_mapping("<leader>f", {
-            format_opts = {
-                async = false,
-                timeout_ms = 10000,
-            },
-            servers = {
-                ["rust_analyzer"] = { "rust" },
-                ["null-ls"] = {
-                    "css",
-                    "javascript",
-                    "javascriptreact",
-                    "typescript",
-                    "typescriptreact",
-                    "json",
-                    "scss",
-                    "less",
-                    "tsx",
-                    "lua",
-                    "svelte",
-                    "ruby",
-                    "go",
-                    "arduino",
-                },
-            },
-        })
+    ls.filetype_extend("ruby", { "rails" })
+    ls.filetype_extend("javascript", { "typescript" })
+    ls.filetype_extend("arduino", { "cpp" })
 
-        lsp.setup()
+    cmp.setup({
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "buffer", keyword_length = 5 },
+      },
+      mapping = {
+        ["<Tab>"] = cmp_action.tab_complete(),
+        ["<S-Tab>"] = cmp_action.select_prev_or_fallback(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      },
+      sorting = {
+        comparators = {
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
 
-        local null_ls = require("null-ls")
+          -- copied from cmp-under, but I don't think I need the plugin for this.
+          -- I might add some more of my own.
+          function(entry1, entry2)
+            local _, entry1_under = entry1.completion_item.label:find("^_+")
+            local _, entry2_under = entry2.completion_item.label:find("^_+")
+            entry1_under = entry1_under or 0
+            entry2_under = entry2_under or 0
+            if entry1_under > entry2_under then
+              return false
+            elseif entry1_under < entry2_under then
+              return true
+            end
+          end,
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.rubocop,
-                null_ls.builtins.formatting.astyle,
-                null_ls.builtins.formatting.prettierd.with({
-                    filetypes = {
-                        "css",
-                        "javascript",
-                        "javascriptreact",
-                        "typescript",
-                        "typescriptreact",
-                        "json",
-                        "scss",
-                        "less",
-                        "tsx",
-                        "svelte",
-                    },
-                }),
-            },
-            autostart = true,
-        })
-    end,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      },
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+    })
+
+    lsp.format_mapping("<leader>f", {
+      format_opts = {
+        async = false,
+        timeout_ms = 10000,
+      },
+      servers = {
+        ["rust_analyzer"] = { "rust" },
+        ["null-ls"] = {
+          "css",
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "json",
+          "scss",
+          "less",
+          "tsx",
+          "lua",
+          "svelte",
+          "ruby",
+          "go",
+          "arduino",
+          "yaml",
+        },
+      },
+    })
+
+    lsp.setup()
+
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.formatting.astyle,
+        null_ls.builtins.code_actions.shellcheck.with({
+          filetypes = { "sh", "zsh", "bash" },
+        }),
+        null_ls.builtins.formatting.prettierd.with({
+          filetypes = {
+            "css",
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "json",
+            "scss",
+            "less",
+            "tsx",
+            "svelte",
+          },
+        }),
+      },
+      autostart = true,
+    })
+  end,
 }
